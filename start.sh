6f9b4dbc453b2fb8ea27a17d8f021ed23da68bd2
Alice-Githui <gnyokabi.alice@gmail.com>
2021-04-26 20:59:44 +0300

    Push to heroku

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 export SECRET_KEY='Flask App Secret Key'
 export MAIL_USERNAME='techinthings0@gmail.com'
 export MAIL_PASSWORD=123#ytrewq
+export DATABASE_URL="postgres://mcgbilkfqxonto:5fc04aa9b4c26337dd5b45a7d5307cb732c41e5cc27aff82b7beff78655bbc99@ec2-52-87-107-83.compute-1.amazonaws.com:5432/d6llg3ojpk3s51?sslmode=require"
 
 python3.9 manage.py server
