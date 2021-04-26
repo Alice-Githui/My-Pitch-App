db2356792319b472dd829445306b20ea2b5c542f
Alice-Githui <gnyokabi.alice@gmail.com>
2021-04-26 16:09:01 +0300

    Deploy to heroku

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ class ProdConfig(Config):
     Args:
     Config: The parent configuration with General configuration settings
     '''
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://githui:Kqcaptain#2@localhost/pitch_test'
